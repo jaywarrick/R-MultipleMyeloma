90ef7e2cfb3e53bed1bcd6fb805f42de8aa9ae2d
jaywarrick <jaywarrick@Warrick-Work.local>
2015-12-02 03:50:40 -0600

    Generate plots of Dom data for WARF 2020 grant

--- PreProcess.R
+++ PreProcess.R
@@ -4,11 +4,15 @@ source('~/.Rprofile')
 # Also export data with "Class" column
 # Also export X Y data for contextual analysis.
 
+folder <- '/Users/jaywarrick/Documents/MMB/Grants/2015 - Wisconsin 2020/Miyamoto UW2020'
+w <- 5
+h <- 5
+
 # First read in the data
-duh <- read.arff("/Volumes/JEX Cruncher/JEX Databases/Dominique/Mutant vs WT/Cell_x0_y0/File-Output ARFF Table/x0_y0.arff")
+duh <- read.arff("/Volumes/BeebeBig/Jay/JEX Databases/Dominique/Mutant vs WT/Cell_x0_y0/File-Output ARFF Table/x0_y0.arff")
 duh$Class <- "Mutant"
 data <- duh
-duh <- read.arff("/Volumes/JEX Cruncher/JEX Databases/Dominique/Mutant vs WT/Cell_x1_y0/File-Output ARFF Table/x1_y0.arff")
+duh <- read.arff("/Volumes/BeebeBig/Jay/JEX Databases/Dominique/Mutant vs WT/Cell_x1_y0/File-Output ARFF Table/x1_y0.arff")
 duh$Class <- "WT"
 data <- rbind(data, duh)
 
@@ -48,5 +52,90 @@ data$Class <- as.factor(classes)
 
 
 write.arff(data, '/Users/jaywarrick/Desktop/TestOutput.arff')
+data <- read.arff('/Users/jaywarrick/Desktop/TestOutput.arff')
+
+MT <- subset(data, Class=='Mutant')
+WT <- subset(data, Class=='WT')
+hist(subset(data, Class=='WT' & firstorder.mean.650X705M < 550 & firstorder.mean.650X705M > 490)$firstorder.mean.650X705M, breaks=seq(490,550,1), add=FALSE, col='red', xlab='p65 Intensity', main='')
+hist(subset(data, Class=='Mutant' & firstorder.mean.650X705M < 550 & firstorder.mean.650X705M > 490)$firstorder.mean.650X705M, breaks=seq(490,550,1), add=TRUE, col='blue')
+
+pdf(file=file.path(folder,'Area.pdf'), w=w, h=h)
+hist(WT$geometric.Area.NA, add=FALSE, breaks=seq(0,100000,300), col='red', xlab='Area', main='', xlim=c(0,5000), ann=F)
+hist(MT$geometric.Area.NA, add=TRUE, breaks=seq(0,100000,300), col='blue')
+dev.off()
+
+
+pdf(file=file.path(folder,'Area.pdf'), w=w, h=h)
+hist(WT$imagemoment.NormalizedCentralMoment12.485X525M, add=FALSE, breaks = 400, col='red', xlab='Area', main='', ann=F)
+hist(MT$imagemoment.NormalizedCentralMoment12.485X525M, add=TRUE, breaks = 400, col='blue')
+dev.off()
+
+t.test(WT$geometric.Area.NA, MT$geometric.Area.NA)
+
+x1 <- MT$imagemoment.CentralMoment11.650X705M/1e15 + rnorm(MT$imagemoment.CentralMoment11.650X705M, mean=0, sd=0.03)
+y1 <- MT$imagemoment.HuMoment3.485X525M * rnorm(MT$imagemoment.HuMoment3.485X525M, mean=1, sd=0.12)
+x2 <- WT$imagemoment.CentralMoment11.650X705M/1e15 + rnorm(WT$imagemoment.CentralMoment11.650X705M, mean=0, sd=0.03)
+y2 <- WT$imagemoment.HuMoment3.485X525M * rnorm(WT$imagemoment.HuMoment3.485X525M, mean=1, sd=0.12)
+pdf(file=file.path(folder,'CentralMoment11vsHuMomment3.pdf'), w=w, h=h)
+plot(x1, y1, pch=21, cex=0.5, col=rgb(0,0,1,0.2), bg=rgb(0,0,1,0.2), xlim=c(-1.7,0.5), xlab='RELA (p65), Central Moment 11', ylab='CellTracker Green, Hu Moment 3', ann=F, log='y')
+points(x2, y2, pch=21, cex=0.5, col=rgb(1,0,0,0.2), bg=rgb(1,0,0,0.2))
+dev.off()
+
+imagemoment.CentralMoment21.485X525M
+x1 <- MT$imagemoment.HuMoment1.485X525M# * rnorm(MT$imagemoment.HuMoment1.650X705M, mean=1, sd=0.01)
+y1 <- MT$imagemoment.HuMoment3.485X525M# * rnorm(MT$imagemoment.HuMoment3.485X525M, mean=1, sd=0.01)
+x2 <- WT$imagemoment.HuMoment1.485X525M# * rnorm(WT$imagemoment.HuMoment1.650X705M, mean=1, sd=0.01)
+y2 <- WT$imagemoment.HuMoment3.485X525M# * rnorm(WT$imagemoment.HuMoment3.485X525M, mean=1, sd=0.01)
+pdf(file=file.path(folder,'HuMoment1vsHuMomment3.pdf'), w=w, h=h)
+plot(x1, y1, pch=21, cex=0.5, col=rgb(0,0,1,0.2), bg=rgb(0,0,1,0.2), xlim=c(0,0.00005), ylim=c(1e-6, 1e-5), xlab='RELA (p65), Central Moment 11', ylab='CellTracker Green, Hu Moment 3', ann=F, log='y')
+plot(x1, y1, pch=21, cex=0.5, col=rgb(0,0,1,0.2), bg=rgb(0,0,1,0.2),  xlab='RELA (p65), Central Moment 11', ylab='CellTracker Green, Hu Moment 3', ann=F, log='y')
+points(x2, y2, pch=21, cex=0.5, col=rgb(1,0,0,0.2), bg=rgb(1,0,0,0.2))
+dev.off()
+
+x1 <- MT$imagemoment.HuMoment1.650X705M# * rnorm(MT$imagemoment.HuMoment1.650X705M, mean=1, sd=0.01)
+y1 <- MT$imagemoment.HuMoment3.485X525M# * rnorm(MT$imagemoment.HuMoment3.485X525M, mean=1, sd=0.01)
+x2 <- WT$imagemoment.HuMoment1.650X705M# * rnorm(WT$imagemoment.HuMoment1.650X705M, mean=1, sd=0.01)
+y2 <- WT$imagemoment.HuMoment3.485X525M# * rnorm(WT$imagemoment.HuMoment3.485X525M, mean=1, sd=0.01)
+pdf(file=file.path(folder,'HuMoment1vsHuMomment3.pdf'), w=w, h=h)
+plot(x1, y1, pch=21, cex=0.5, col=rgb(0,0,1,0.2), bg=rgb(0,0,1,0.2), xlim=range(x1), xlab='RELA (p65), Central Moment 11', ylab='CellTracker Green, Hu Moment 3', ann=F, log='xy')
+points(x2, y2, pch=21, cex=0.5, col=rgb(1,0,0,0.2), bg=rgb(1,0,0,0.2))
+dev.off()
+
+x1 <- MT$firstorder.mean.650X705M
+y1 <- MT$firstorder.mean.485X525M
+x2 <- WT$firstorder.mean.650X705M
+y2 <- WT$firstorder.mean.485X525M
+n1 <- length(x1)
+n2 <- length(x2)
+pdf(file=file.path(folder,'Intensity.pdf'), w=w, h=h)
+plot(x2-500, y2-500, pch=21, cex=0.5, cex.axis=1, col=rgb(0,0,1,0.2), bg=rgb(0,0,1,0.2), xlim=c(1,75), ylim=c(1,1500), xlab='RELA (p65), Central Moment 11', ylab='CellTracker Green, Hu Moment 3', ann=F, log='xy')
+points(x1-500, y1-500, pch=21, cex=0.5, cex.axis=1, col=rgb(1,0,0,0.4), bg=rgb(1,0,0,0.4))
+dev.off()
+
+# Blank plot
+pdf(file=file.path(folder,'BlankPlot.pdf'), w=3.7, h=h)
+plot(c(),c(), xlab='', ylab='', xlim=c(-0.5,4.5), ylim=c(0, 1.2), log='', xaxt='n')
+axis(1,at=c(0,1,2,3,4), labels=c(0,100,1,10,100))
+dev.off()
+
+# Blank plot
+pdf(file=file.path(folder,'BlankMLCurve.pdf'), w=3.7, h=h)
+plot(c(),c(), xlab='', ylab='', xlim=c(0,50), ylim=c(0, 100), log='')
+dev.off()
+
+plot(x, y, pch=21, cex=0.5, col=rgb(0,0,1,0.2), bg=rgb(0,0,1,0.2), xlim=range(data$imagemoment.CentralMoment11.650X705M), xlab='RELA (p65), Central Moment 11', ylab='CellTracker Green, Hu Moment 3', xaxt='n')
+points(WT$imagemoment.CentralMoment11.650X705M/1e15, WT$imagemoment.HuMoment3.485X525M, pch=21, cex=0.5, col=rgb(1,0,0,0.2), bg=rgb(1,0,0,0.2))
+axis(1, at=c(-1e-5, 0, 1e5, 1e10, 1e15))
+MTdia <- sqrt(MT$geometric.Area.NA/pi)*(6.45/40)*2
+WTdia <- sqrt(WT$geometric.Area.NA/pi)*(6.45/40)*2
+mad(MTdia)/median(MTdia)
+mad(WTdia)/median(WTdia)
+(median(MTdia)-median(WTdia))/(median(MTdia))
+
+MTp65 <- MT$firstorder.mean.650X705M
+WTp65 <- WT$firstorder.mean.650X705M
+mad(MTp65)/median(MTp65)
+mad(WTp65)/median(WTp65)
+(median(WTp65)-median(MTp65))/(median(WTp65))
 
 # Try a simple Naive Bayes Classifier
