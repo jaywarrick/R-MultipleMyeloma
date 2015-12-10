0baaf913fad5d2521880e30a59379a2b87f138e5
jaywarrick <jaywarrick@Warrick-Work.local>
2015-12-10 13:09:47 -0600

    Initial commit

---
+++ NewLiveDead.R
@@ -0,0 +1,184 @@
+analyzeLiveDead <- function(compiledTablePath, jexFolder, logRatioThreshold=0, nClusters=2, locationDimension='Location')
+{
+     # Load necessary libraries
+     library(foreign)
+     library(data.table)
+     library(EMCluster)
+
+     # Define some helper functions
+     plotHist <- function(data, LDClass, thresh, jexFolder, x, y, loc, prefix)
+     {
+          imagePath <- file.path(jexFolder,paste0(prefix, x, '_', y, '_', loc, '.tif'))
+          tiff(filename=imagePath)
+          add <- FALSE
+          col <- list(red=0, green=0, blue=0, alpha=0.5)
+          tempHist <- hist(data, breaks=20, plot=FALSE)
+          myBreaks <- tempHist$breaks
+          myLim <- max(tempHist$counts)
+          for(i in unique(LDClass))
+          {
+               if(length(data[LDClass==i])>0)
+               {
+                    tempCol <- col
+                    tempCol[(i%%3)+1] <- 1
+                    freshCol <- do.call(rgb, tempCol)
+                    if(add)
+                    {
+                         rData <- range(data)
+                         hist(data[LDClass==i], breaks=myBreaks, main=paste0('X:', x, ' Y:', y, ' Loc:', loc), col=freshCol, add=add, freq=TRUE, ylim=c(0,myLim))
+                    }
+                    else
+                    {
+                         hist(data[LDClass==i], breaks=myBreaks, main=paste0('X:', x, ' Y:', y, ' Loc:', loc), col=freshCol, add=add, freq=TRUE, ylim=c(0,myLim))
+                    }
+                    add <- TRUE
+               }
+          }
+          abline(v=thresh, lwd=2, col='blue')
+          dev.off()
+          return(imagePath)
+     }
+
+     plotHistAll <- function(data, LDClass, thresh, jexFolder, isLog, prefix)
+     {
+          imagePath <- file.path(jexFolder,paste0(prefix, '.tif'))
+          tiff(filename=imagePath)
+          if(isLog)
+          {
+               add <- FALSE
+               col <- list(red=0, green=0, blue=0, alpha=0.5)
+               tempHist <- hist(data, breaks=40, plot=FALSE)
+               myBreaks <- tempHist$breaks
+               myLim <- max(tempHist$counts)
+               for(i in unique(LDClass))
+               {
+                    if(length(data[LDClass==i])>0)
+                    {
+                         tempCol <- col
+                         tempCol[(i%%3)+1] <- 1
+                         freshCol <- do.call(rgb, tempCol)
+                         hist(data[LDClass==i], breaks=myBreaks, main='log(Ratio) All', col=freshCol, add=add, freq=TRUE, ylim=c(0,myLim))
+                         add <- TRUE
+                    }
+               }
+          }
+          else
+          {
+               add <- FALSE
+               col <- list(red=0, green=0, blue=0, alpha=0.5)
+               tempHist <- hist(data, breaks=40, plot=FALSE)
+               myBreaks <- tempHist$breaks
+               myLim <- max(tempHist$counts)
+               for(i in unique(LDClass))
+               {
+                    if(length(data[LDClass==i])>0)
+                    {
+                         tempCol <- col
+                         tempCol[(i%%3)+1] <- 1
+                         freshCol <- do.call(rgb, tempCol)
+                         hist(data[LDClass==i], breaks=myBreaks, main='Ratio All', col=freshCol, add=add, freq=TRUE, ylim=c(0,myLim))
+                         add <- TRUE
+                    }
+               }
+          }
+          abline(v=thresh, lwd=2, col='blue')
+          dev.off()
+          return(imagePath)
+     }
+
+     assignToClusters <- function(data, nClusters=2, rndSeed=1234)
+     {
+          set.seed(rndSeed)
+          yo <- data[!is.na(data)]
+          if(length(yo) > 10000)
+          {
+               x <- data.frame(x=sample(yo, 10000))
+          }
+          else
+          {
+               x <- data.frame(x=yo)
+          }
+
+
+          emobj <- simple.init(x, nclass = nClusters)
+          control <- .EMControl(alpha = 0.99, short.iter = 200, short.eps = 1e-2,
+                     fixed.iter = 1, n.candidate = 3,
+                     EM.iter = 100, EM.eps = 1e-3, exhaust.iter = 5)
+          ret <- emcluster(x, emobj, assign.class = TRUE, EMC=control)
+
+          temp <- data.frame(x=x$x, LDClass=ret$class)
+          tempMu <- data.frame(mu=as.vector(ret$Mu), LDClass=1:nrow(ret$Mu))
+          tempMu <- tempMu[order(tempMu$mu),]
+          temp2 <- temp
+          tempMu2 <- tempMu
+          for(i in 1:nrow(tempMu))
+          {
+               temp[temp2$LDClass==tempMu$LDClass[i],]$LDClass <- i
+               tempMu2$LDClass[i] <- i
+          }
+
+          temp$LDClass <- temp$LDClass-1
+
+          thresh <- list()
+          ret <- data.frame(x=data, LDClass=nrow(tempMu)-1)
+          for(i in (nrow(tempMu)-1):1)
+          {
+               tempThresh <- min(temp[temp$LDClass == i,'x'])
+               if(!length(tempThresh)==0)
+               {
+                    thresh[i] <- tempThresh[1]
+                    ret$LDClass[ret$x < tempThresh[1]] <- i-1
+               }
+          }
+
+          return(list(data=ret, mu=tempMu$mu, thresh=thresh))
+     }
+
+     # Read in the file
+     duh <- read.arff(compiledTablePath)
+     duh <- reorganizeFeatureTable(duh, specialNames=c(), convertToNumeric=FALSE)
+     duh <- data.frame(duh)
+
+     #Calculate the live/dead signal ratio
+     duh$ratio <- (duh$G + 101) / (duh$R+101) # this is to avoid division by 0 and assumes the background correction step adds back 100 intensity units to images.
+     duh$logRatio <- log(duh$ratio)
+
+     # Assign cells as either live (1) or dead (0)
+     duh$LD <- 1
+     duh$LD[duh$logRatio < logRatioThreshold] <- 0
+
+     # Attempt EM Clustering to determine live / dead
+     results <- assignToClusters(duh$logRatio, nClusters=nClusters)
+     hist(duh$logRatio)
+     duh$LDClass <- results$data$LDClass
+
+     # Plot the data by array location and
+     duh <- data.table(duh)
+     if(locationDimension != '')
+     {
+          temp1 <- duh[ , plotHist(data=ratio, LDClass=LDClass, jexFolder=jexFolder, thresh=exp(logRatioThreshold), x=Array.X[1], y=Array.Y[1], loc=.BY[[4]], prefix='hist1_'), by=c('Array.X', 'Array.Y', 'Experiment', locationDimension)]
+          temp1 <- rbind(temp1, duh[ , plotHist(data=logRatio, LDClass=LDClass, jexFolder=jexFolder, thresh=logRatioThreshold, x=Array.X[1], y=Array.Y[1], loc=.BY[[4]], prefix='hist2_'), by=c('Array.X', 'Array.Y', 'Experiment', locationDimension)])
+     }
+     else
+     {
+          temp1 <- duh[ , plotHist(data=ratio, LDClass=LDClass, jexFolder=jexFolder, thresh=exp(logRatioThreshold), x=Array.X[1], y=Array.Y[1], loc='', prefix='hist1_'), by=c('Array.X', 'Array.Y', 'Experiment')]
+          temp1 <- rbind(temp1, duh[ , plotHist(data=logRatio, jexFolder=jexFolder, thresh=logRatioThreshold, LDClass=LDClass, x=Array.X[1], y=Array.Y[1], loc='', prefix='hist2_'), by=c('Array.X', 'Array.Y', 'Experiment')])
+     }
+     temp2 <- duh[ , plotHistAll(data=ratio, LDClass=LDClass, jexFolder=jexFolder, thresh=exp(logRatioThreshold), isLog=FALSE, prefix='hist1_All'), ]
+     temp2 <- rbind(temp2, duh[ , plotHistAll(data=logRatio, LDClass=LDClass, jexFolder=jexFolder, thresh=logRatioThreshold, isLog=TRUE, prefix='hist2_All'), ])
+
+     imageList1 <- as.vector(temp1$V1)
+     imageList2 <- as.vector(temp2)
+
+     summaryTable <- data.frame()
+     summaryTable <- duh[ , list(LDRatio_Threshold=sum(LD)/length(LD), LDRatio_Cluster=sum(LDClass)/length(LDClass)), by=.(Array.X,Array.Y,Experiment,Location)]
+     path1 <- file.path(jexFolder,'SummaryTable.csv')
+     write.csv(x=summaryTable,file=path1)
+     path2 <- file.path(jexFolder,'SingleCellTable.csv')
+     write.csv(x=duh,file=path2)
+     fileList1 <- c(path1)
+     fileList2 <- c(path2)
+     return(list(image1=imageList1, image2=imageList2, file1=fileList1, file2=fileList2, ret=duh, clusterResults=results))
+}
+
+# something potentially good to know for this file blah<-'b', blah2<-'a', temp[,mean(unlist(.SD[,'a',with=FALSE])),by=c(blah)]
---
+++ TestNewLiveDead.R
@@ -0,0 +1,26 @@
+library(data.table)
+library(foreign)
+base1 <- 500
+base2 <- 1000
+nm <- 2
+nl <- 3
+ny <- 3
+n1 <- base1*ny*nl*nm
+n2 <- base2*ny*nl*nm
+n <- n1+n2
+mu1 <- -1
+mu2 <- 1
+x1 <- rnorm(n1, mean=mu1, sd=0.6)
+x2 <- rnorm(n2, mean=mu2, sd=0.6)
+x <- sample(c(x1,x2), size=(n/2))
+tempData <- data.frame(Id=rep(rep(1:(base1+base2),each=nl*ny),nm), Extra=5, Experiment='hello', Array.X=0, Array.Y=1:ny, Location=rep(1:nl, each=ny), Measurement=rep(c('R','G'), each=n/nm), Value=c(rep(500,n/2), sample(500*exp(x),n/2)))
+tempData <- data.table(tempData)
+tempData <- tempData[,lapply(.SD, as.factor)]
+tempData$Value <- as.numeric(as.character(tempData$Value))
+write.arff(tempData, '/Users/jaywarrick/Desktop/A Sandbox/madeUpData.arff')
+
+hist(sample(exp(x),n/2))
+hist(log(sample(exp(x),n/2)))
+results <- analyzeLiveDead(logRatioThreshold=1.2, nClusters=2, locationDimension="", compiledTablePath='/Users/jaywarrick/Desktop/A Sandbox/madeUpData.arff', jexFolder='/Users/jaywarrick/Desktop/A Sandbox')
+
+# logRatio = log(G/R) = x -> G/R = exp(x) -> G = R*exp(x) -> R = G/exp(x)
